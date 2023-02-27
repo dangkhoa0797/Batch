566a181d5a93bed609f7382c7f1726eb159b2992
khoahd <hoangdangkhoa0797@gmail.com>
2023-02-27 09:00:31 +0700

    kh

---
+++ WriteText.bat
@@ -0,0 +1,9 @@
+rem @echo on/off bật tắt hiện lệnh trên màn hình, mặc định là on
+Title Writing Using Batch Files
+color 0a
+cd /d D:\Batch\Text
+echo khoa > khoa1.txt
+echo khoa >> khoa2.txt
+dir
+cd /d D:\Batch\
+rem cd /d C:\
